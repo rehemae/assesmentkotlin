ef69031948d6fe1bd595e9ec311a1dcab84d002e
rehemae <rehemaephraimnanda@gmail.com>
2022-03-10 08:55:02 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,24 @@
+fun main(){
+    var num=getReminder(10,9)
+    println(num)
+    var name="zawadi"
+    println(name)
+
+
+}
+fun getReminder(num1: Int ,num2: Int):Int {
+    var num = num1 % num2
+    return num
+
+
+}
+
+    fun details(name: String): String {
+        println(name.length)
+
+
+
+    }
+
+
+
